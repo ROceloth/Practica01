9c709af51e787e6c36557314287dda838002ccaa
ROceloth <47311509+ROceloth@users.noreply.github.com>
2019-08-13 23:20:51 -0500

    Clase  C con test
    Se junto las clases A, B y C, solo la clase C tiene una funcion de test con lectura y salida estandar

--- src/B.py
+++ src/B.py
@@ -4,5 +4,3 @@ class B(A):
     def metodoB(self, palabra):
         return self.str + ' ' + palabra
 
-b = B('clase 2')
-print(b.metodoB('hola'))
--- src/C.py
+++ src/C.py
@@ -3,3 +3,11 @@ from B import B
 class C(B):
     def metodoC(self, x:int) -> str:
         return self.str * x
+
+"""test"""
+
+str = input("Escribe una palabra ")
+x = int(input('Escribe un numero '))
+
+c = C(str)
+print(c.metodoC(x))
